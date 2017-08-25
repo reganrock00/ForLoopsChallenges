4cce13cf34398d33b23f6b070d32a3a7ccf13fc0
Regan.Archibald <Regan.Archibald@MU-C1-14.mullen.pvt.k12.co.us>
2017-08-25 09:09:08 -0600

    secondary commit

--- For Loops Challenges/src/ForLoopsChallenges.java
+++ For Loops Challenges/src/ForLoopsChallenges.java
@@ -13,12 +13,30 @@ public class ForLoopsChallenges
 
 	private static void Donuts() 
 	{
-		
+		int x =0;
+		for (x = 0; x < 5; x++)
+			{
+				System.out.println("I love donuts!");
+			}
+		System.out.println(" ");
 	}
 
 	private static void Ascending() 
 	{
-		
+		int number = 0;
+		for ( int i = 0; i < 10; i++)
+			{
+				if (i < 5)
+					{
+						number = i + 1;
+					}
+				if (i > 4)
+					{
+						number = 10 - i;
+					}
+				System.out.println(number);
+			}
+		System.out.println(" ");
 	}
 
 	private static void Multiples() 
