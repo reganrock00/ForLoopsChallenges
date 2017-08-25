4c0b4f3bc7ba4602fa26cb2ce6b0ef4f93212c92
CyberPower <CyberPower@10.0.0.48>
2017-08-24 21:37:56 -0600

    first commit outline

---
+++ For Loops Challenges/src/ForLoopsChallenges.java
@@ -0,0 +1,39 @@
+
+public class ForLoopsChallenges 
+{
+
+	public static void main(String[] args) 
+	{
+		Donuts();
+		Ascending();
+		Multiples();
+		Range();
+		Draw();
+	}
+
+	private static void Donuts() 
+	{
+		
+	}
+
+	private static void Ascending() 
+	{
+		
+	}
+
+	private static void Multiples() 
+	{
+		
+	}
+
+	private static void Range() 
+	{
+		
+	}
+
+	private static void Draw() 
+	{
+		
+	}
+
+}
